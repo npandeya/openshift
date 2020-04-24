44b24e8eb145c1f54ba187dafa2c8bb18bf6e4d7
Nimesh <nimeshdevops@gmail.com>
2020-04-24 10:05:51 -0500

    changed app

--- app.py
+++ app.py
@@ -1,3 +1,14 @@
-print ('hello world')
-Moderator="Nimesh"
-print ('hello ' + Moderator)
+import os
+from flask import Flask 
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+    return "Welcome!"
+
+@app.route('/how are you')
+def hello():
+    return 'I am good, how about you?'
+
+if __name__ =="__main__":
+    app.run(host="0.0.0.0", port=8080)
