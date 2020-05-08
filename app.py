eed9ccddb167e5525e03bd6e4976d8a70aa6f9b3
Nimesh <nimeshdevops@gmail.com>
2020-05-08 12:59:20 -0500

    change may 8

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return "Welcome! THIS IS THE CHANGE TO AUTOMATE BUILD IN OPENSHIFT - change 3 for auto deploy --THIS IS END TO END TEST"
+    return "Welcome! THIS IS THE CHANGE TO AUTOMATE BUILD IN OPENSHIFT - change 3 for auto deploy --THIS IS END TO END TEST -  Change 1"
 
 @app.route('/how are you')
 def hello():
